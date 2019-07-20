b8df637b684793bee531d8269d1b73f10ee4a6e7
Alexander Khovansky <alex@wovn.io>
2019-07-21 00:59:40 +0900

    Added code sample

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,24 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 export default {
-    input: 'build/esm/index.js',
-    output: {
-        file: './build/index.js',
-        format: 'iife',
-        sourcemap: true
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        sourcemaps(),
-        terser()
-    ]
+  input: 'build/esm/index.js',
+  output: {
+    file: './build/index.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    sourcemaps(),
+    terser(),
+    copy({
+      targets: [
+        { src: ['src/index.html', 'src/test-code.png'], dest: 'build/' }
+      ],
+    }),
+  ]
 }
